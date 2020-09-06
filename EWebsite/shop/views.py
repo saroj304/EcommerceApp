8bbbfee09a5139a086072635adfffee3a7f0056d
saroj <sarojkhatiwada1999@gmail.com>
2020-09-06 23:52:14 +0545

    firstcommitted

---
+++ EWebsite/shop/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def index(request):
+    return render(request,'shop/index.html')
+def about(request):
+    return HttpResponse("about page")
+def contact(request):
+    return HttpResponse("contact page")
+def tracker(request):
+    return HttpResponse("tracker page")
+def search(request):
+    return HttpResponse("search page")
+def productview(request):
+    return HttpResponse("product page")
+def checkout(request):
+    return HttpResponse("checkout page")
